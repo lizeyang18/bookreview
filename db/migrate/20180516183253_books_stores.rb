ff321217f4114c3b9eb14e08095b8d3e23da1342
IZYROX <kotroczoroland92@gmail.com>
2018-05-17 02:19:37 +0200

    final commit#2

--- db/migrate/20180516183253_books_stores.rb
+++ db/migrate/20180516183253_books_stores.rb
@@ -4,18 +4,6 @@ class BooksStores < ActiveRecord::Migration[5.2]
       t.belongs_to :book, index: true
       t.belongs_to :store, index: true
     end
-
-
-    execute "INSERT INTO books_stores values(9, 1)"
-    execute "INSERT INTO books_stores values(9, 2)"
-    execute "INSERT INTO books_stores values(9, 3)"
-    execute "INSERT INTO books_stores values(10, 2)"
-    execute "INSERT INTO books_stores values(10, 3)"
-    execute "INSERT INTO books_stores values(10, 4)"
-    execute "INSERT INTO books_stores values(11, 1)"
-    execute "INSERT INTO books_stores values(11, 1)"
-    execute "INSERT INTO books_stores values(11, 2)"
-    execute "INSERT INTO books_stores values(11, 3)"
   end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-#
+[Book, Category, Review, Store].each(&:delete_all)
+
+@index = 1
+Book.populate 5 do |book|
+  book.title        = Faker::Book.title
+  book.description  = Faker::Lorem.paragraph(5)
+  book.author       = Faker::Book.author
+  book.user_id      = Faker::Number.digit
+  book.category_id  = Faker::Number.digit
+  book.book_img_file_name = "book_" + @index.to_s + ".jpg"
+  @index = @index + 1
+  book.book_img_content_type =  "image/jpeg"
+  Review.populate 1..6 do |review|
+    review.rating   = Faker::Number.digit
+    review.comment  = Faker::Lorem.sentence(1)
+    review.book_id  = book.id
+  end
+end
+
+
+Category.populate 4 do |category|
+  category.name     = Faker::Book.genre
+end
+
+Store.populate 4 do |store|
+  store.name     = Faker::Lorem.sentences(120)
+end
