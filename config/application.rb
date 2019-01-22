68cfb8e0001ceb8c770df895feb9b1a02a9fc9ef
wangchanghui <wangchanghui18@mails.ucas.ac.cn>
2019-01-22 03:50:02 +0000

    solve star

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,10 @@ module BookReviewApp
     config.load_defaults 5.2
     config.i18n.default_locale = :hu
     config.serve_static_assets = false
-
+    #
+    config.assets.initialize_on_precompile = false
+    #config.i18n.fallbacks = true
+    config.i18n.fallbacks = [I18n.default_locale]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
